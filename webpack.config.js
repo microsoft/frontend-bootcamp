1c5b6cc9a77df8622d1b2ba4977533c32840ebc7
Ken <kchau@microsoft.com>
2019-01-18 10:28:56 -0800

    make it export to the right output folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,36 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: {
-    step01: './step01/index.js',
-    step02: './step02/src/index.ts'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js']
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, '[name]/dist')
-  }
+const entries = {
+  step01: './step01/index.js',
+  step02: './step02/src/index.ts'
 };
+
+module.exports = Object.keys(entries).map(entryPoint => {
+  const entryRequest = entries[entryPoint];
+  return {
+    entry: { [entryPoint]: entryRequest },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'public/index.html'),
+        filename: '../index.html'
+      })
+    ],
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, entryPoint, 'dist')
+    }
+  };
+});
