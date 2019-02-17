64bd766619146f19d0c8aaa44e3b10b4ab3fc5de
Ken Chau <ken@gizzar.com>
2019-02-16 17:06:30 -0800

    adding some comments about devtool hook up

--- step2-06/src/index.tsx
+++ step2-06/src/index.tsx
@@ -2,10 +2,14 @@ import { reducer } from './reducers';
 import { createStore, compose } from 'redux';
 import { addTodo } from './actions';
 
+/* Goop for making the Redux dev tool to work */
 declare var window: any;
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+function createStoreWithDevTool(reducer, initialStore) {
+  return createStore(reducer, initialStore, composeEnhancers());
+}
 
-const store = createStore(reducer, {}, composeEnhancers());
+const store = createStoreWithDevTool(reducer, {});
 
 console.log(store.getState());
 
