54f3c205f49ade0e5574ca2be97458743374bb34
Micah Godbolt <mgodbolt@microsoft.com>
2019-02-25 10:12:40 -0800

    removing docs from master branch

--- docs/step1-05/demo/src/components/TodoFooter.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const TodoFooter = (props: any) => {
-  return (
-    <footer>
-      <p>footer</p>
-    </footer>
-  );
-};
--- docs/step1-05/demo/src/components/TodoHeader.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export class TodoHeader extends React.Component {
-  render() {
-    return <div />;
-  }
-}
