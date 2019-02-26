d8702ec24e3bc849398f18a9b4e6878160915a1d
Micah Godbolt <mgodbolt@microsoft.com>
2019-02-26 11:21:28 -0800

    misc fixes

--- step1-06/demo/src/components/TodoFooter.tsx
+++ step1-06/demo/src/components/TodoFooter.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const TodoFooter = (props: any) => {
-  const itemCount = Object.keys(props.todos).filter(id => !props.todos[id].completed).length;
+  // const itemCount = Object.keys(props.todos).filter(id => !props.todos[id].completed).length;
   return (
     <footer>
       <span>4 items left</span>
