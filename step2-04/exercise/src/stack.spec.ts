632bb4f210d7ef91d6d047ce6c31973259ec587e
Elizabeth Craig <elcraig@microsoft.com>
2019-02-28 15:22:20 -0800

    Update step2-04 exercises

--- step2-04/exercise/src/stack.spec.ts
+++ step2-04/exercise/src/stack.spec.ts
@@ -1,15 +1,17 @@
-describe('stack', () => {
+// Import the stack here
+
+describe('Stack', () => {
   it('should push item to the top of the stack', () => {
     // implement test here:
-    // 1. require the stack
-    // 2. create stack push calls to place some items in the stack
-    // 3. write assertions via the expect() API
+    // 1. Create a stack instance
+    // 2. Use stack push calls to add some items to the stack
+    // 3. Write assertions via the expect() API
   });
 
   it('should pop the item from the top of stack', () => {
     // implement test here:
-    // 1. require the stack
-    // 2. create stack push calls to place some items in the stack
+    // 1. Create a stack instance
+    // 2. Use stack push calls to add some items to the stack
     // 3. pop a few items off the stack
     // 4. write assertions via the expect() API
   });
