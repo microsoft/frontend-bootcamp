703a366343af21d4e989f76cfba6d2528761756c
Elizabeth Craig <elcraig@microsoft.com>
2019-02-28 09:15:00 -0800

    fix test names

--- step2-04/demo/src/TestMe.spec.tsx
+++ step2-04/demo/src/TestMe.spec.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { TestMe } from './TestMe';
 
-describe('index', () => {
+describe('TestMe Component', () => {
   it('should have a non-clickable component when the original InnerMe is clicked', () => {
     const wrapper = mount(<TestMe name="world" />);
     wrapper.find('#innerMe').simulate('click');
--- step2-04/demo/src/index.spec.tsx
+++ step2-04/demo/src/index.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
 
-describe('TestMe Component', () => {
+describe('index', () => {
   it('placeholder', () => {
   });
 });
