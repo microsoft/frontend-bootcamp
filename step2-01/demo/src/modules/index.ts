61ae8afdf124303d709da91b34a1202e8c9f1099
Elizabeth Craig <elcraig@microsoft.com>
2019-02-27 20:17:08 -0800

    Step 2-01 updates

--- step2-01/demo/src/modules/index.ts
+++ step2-01/demo/src/modules/index.ts
@@ -1,10 +1,10 @@
 // These are named imports from a file relative to this file
 import { namedConst, namedFn, namedObj, namedConstBracket } from './named';
 
-// We can even apply an alias to the named constant
+// We can even give an alias to the named constant
 import { namedConst as c } from './named';
 
-// These are the same instances of the named imports, but gets imported all at the same time under a single object
+// These are the *same instances* of the named imports, but they all get imported inside a single object
 import * as named from './named';
 
 // Print out the exports
@@ -14,11 +14,14 @@ console.log(namedFn());
 console.log(namedObj);
 console.log(namedConstBracket);
 
-// Print out exports through module level import
+// Print out exports from module level import
 console.log(named.namedConst);
 console.log(named.namedFn());
 console.log(named.namedObj);
 console.log(named.namedConstBracket);
+
+// The named and module-level imports reference the same object instances
+console.log(namedObj === named.namedObj); // true
 
 // Default import can be named anything we want as the consumer
 import DefaultClass from './default';
