3fdd80f39a85c5990e37de2ad9f937ff6df4b1d3
Elizabeth Craig <elcraig@microsoft.com>
2019-02-28 08:10:18 -0800

    Steps 2-3 and 2-4

--- step2-04/demo/src/TestMe.spec.tsx
+++ step2-04/demo/src/TestMe.spec.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import { TestMe } from './TestMe';
 
-describe('TestMe Component', () => {
-  it('should have a non-clickable component when the origina InnerMe is clicked', () => {
-    const wrapper = mount(<TestMe name="world" />);
-    wrapper.find('#innerMe').simulate('click');
-    expect(wrapper.find('#innerMe').text()).toBe('Clicked');
+describe('index', () => {
+  it('placeholder', () => {
   });
 });
--- step2-04/demo/src/index.spec.tsx
+++ step2-04/demo/src/index.spec.tsx
@@ -3,7 +3,7 @@ import { mount } from 'enzyme';
 import { TestMe } from './TestMe';
 
 describe('TestMe Component', () => {
-  it('should have a non-clickable component when the origina InnerMe is clicked', () => {
+  it('should have a non-clickable component when the original InnerMe is clicked', () => {
     const wrapper = mount(<TestMe name="world" />);
     wrapper.find('#innerMe').simulate('click');
     expect(wrapper.find('#innerMe').text()).toBe('Clicked');
