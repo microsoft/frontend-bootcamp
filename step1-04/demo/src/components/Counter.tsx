b9089ab8b6ef67a2b53222268f98fa250fbc8618
Micah Godbolt <mgodbolt@microsoft.com>
2019-03-03 20:24:20 -0800

    wrapping up lesson 4

--- step1-04/demo/src/components/Counter.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export class Counter extends React.Component<any, any> {
-  render() {
-    return <p>hello</p>;
-  }
-}
--- step1-04/final/src/App.tsx
+++ step1-04/final/src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Counter } from './components/Counter';
 
-export class App extends React.Component<any, any> {
-  render() {
-    return (
-      <div className="App">
-        <h2>My App</h2>
-        <Counter text="Chickens" />
-        <Counter text="Ducks" />
-      </div>
-    );
-  }
-}
+export const App = props => {
+  return (
+    <div>
+      <Counter text="chickens" />
+      <Counter text="ducks" />
+    </div>
+  );
+};
