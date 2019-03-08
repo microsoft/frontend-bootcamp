c9ac434ee442be30496247438ec59f46a79ca44e
Matt Pham <matt@dreymur.com>
2019-03-07 23:42:51 -0800

    change setState to use function param

--- step2-02/demo/src/components/TodoApp.tsx
+++ step2-02/demo/src/components/TodoApp.tsx
@@ -33,59 +33,69 @@ export class TodoApp extends React.Component<any, Store> {
   }
 
   private _addTodo = label => {
-    const { todos } = this.state;
     const id = index++;
 
-    this.setState({
-      todos: { ...todos, [id]: { label } }
-    });
+    this.setState(({ todos }) => ({
+      todos: { ...todos, [id]: { label, completed: false } }
+    }));
   };
 
   private _remove = id => {
-    const newTodos = { ...this.state.todos };
-    delete newTodos[id];
+    this.setState(({ todos }) => {
+      const newTodos = { ...todos };
+      delete newTodos[id];
 
-    this.setState({
-      todos: newTodos
+      return {
+        todos: newTodos
+      };
     });
   };
 
   private _complete = id => {
-    const newTodos = { ...this.state.todos };
-    newTodos[id].completed = !newTodos[id].completed;
+    this.setState(({ todos }) => {
+      const newTodos = {
+        ...todos,
+        [id]: { ...todos[id], completed: !todos[id].completed }
+      };
 
-    this.setState({
-      todos: newTodos
+      return {
+        todos: newTodos
+      };
     });
   };
 
   private _edit = (id, label) => {
-    const newTodos = { ...this.state.todos };
-    newTodos[id] = { ...newTodos[id], label };
+    this.setState(({ todos }) => {
+      const newTodos = {
+        ...todos,
+        [id]: { ...todos[id], label }
+      };
 
-    this.setState({
-      todos: newTodos
+      return {
+        todos: newTodos
+      };
     });
   };
 
   private _clear = () => {
-    const { todos } = this.state;
-    const newTodos = {};
+    this.setState(({ todos }) => {
+      const newTodos = {};
 
-    Object.keys(this.state.todos).forEach(id => {
-      if (!todos[id].completed) {
-        newTodos[id] = todos[id];
-      }
-    });
+      Object.keys(this.state.todos).forEach(id => {
+        if (!todos[id].completed) {
+          newTodos[id] = todos[id];
+        }
+      });
 
-    this.setState({
-      todos: newTodos
+      return {
+        todos: newTodos
+      };
     });
   };
 
   private _setFilter = filter => {
     this.setState({
-      filter: filter
+      filter
     });
   };
 }
