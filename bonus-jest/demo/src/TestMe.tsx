af095db2199c14ad595d9e6186c35c36cadc6ffd
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-03-09 23:38:27 +0530

    Property 'setState' error rectified.

--- bonus-jest/demo/src/TestMe.tsx
+++ bonus-jest/demo/src/TestMe.tsx
@@ -17,6 +17,9 @@ export const TestMe = (props: TestMeProps) => {
 };
 
 export class InnerMe extends React.Component<TestMeProps, TestMeState> {
+  setState(arg0: { clicked: boolean; }): any {
+    throw new Error("Method not implemented.");
+  }
   state = {
     clicked: false
   };
@@ -24,6 +27,7 @@ export class InnerMe extends React.Component<TestMeProps, TestMeState> {
   onClick = () => {
     this.setState({ clicked: true });
   };
+  props: any;
 
   render() {
     return !this.state.clicked ? (
@@ -31,7 +35,7 @@ export class InnerMe extends React.Component<TestMeProps, TestMeState> {
         Hello {this.props.name}, Click Me
       </div>
     ) : (
-      <div id="innerMe">Clicked</div>
-    );
+        <div id="innerMe">Clicked</div>
+      );
   }
 }
