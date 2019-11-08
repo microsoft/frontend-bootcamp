7cc9c72c4ed59d84fbb5dbfdae7fdc7414045aa4
Ken Chau <kchau@microsoft.com>
2019-11-08 16:16:29 +0000

    adding docs

--- jest.setup.js
+++
@@ -1,5 +0,0 @@
-// setup file
-var enzyme = require('enzyme');
-var Adapter = require('enzyme-adapter-react-16');
-
-enzyme.configure({ adapter: new Adapter() });
--- prettier.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  singleQuote: true,
-  tabWidth: 2,
-  printWidth: 140
-};
